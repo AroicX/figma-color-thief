6fd3d6cf20c5303300b5e1efc33dbdb1d4777a86
oluwadareseyi <oluwadareseyii@gmail.com>
2021-03-06 18:11:00 +0100

    build basic UI functionality

--- src/figma/code.ts
+++ src/figma/code.ts
@@ -3,47 +3,44 @@ const start = Date.now();
 
 // show UI
 figma.showUI(__html__, {
-    width: 450,
-    height: 630
+  width: 480,
+  height: 570
 });
 
 function getContent(selection: any) {
-    switch (selection.type) {
+  switch (selection.type) {
+    case 'RECTANGLE':
+      if (selection.fills && selection.fills[0]?.type === 'IMAGE') {
+        const { imageHash } = selection.fills[0];
 
-        case 'RECTANGLE':
-            if (selection.fills && selection.fills[0]?.type === 'IMAGE') {
-                const {
-                    imageHash
-                } = selection.fills[0];
+        var imageLink;
+        figma
+          .getImageByHash(imageHash)
+          .getBytesAsync()
+          .then(res => {
+            const buffer = Buffer.from(res);
 
-                var imageLink;
-                figma
-                    .getImageByHash(imageHash)
-                    .getBytesAsync()
-                    .then(res => {
-                        const buffer = Buffer.from(res);
+            const base64String = buffer.toString('base64');
 
-                        const base64String = buffer.toString('base64');
+            var imageName = 'test';
+            figma.ui.postMessage({
+              type: 'image',
+              name: `${selection.name || 'test'}.png`,
+              data: base64String
+            });
+            imageLink = `background-image: url('./images/"${imageName}.png')}`;
+          });
 
-                        var imageName = 'test';
-                        figma.ui.postMessage({
-                            type: 'image',
-                            name: `${selection.name || 'test'}.png`,
-                            data: base64String
-                        });
-                        imageLink = `background-image: url('./images/"${imageName}.png')}`;
-                    });
+        console.log(imageLink);
+      } else {
+        figma.closePlugin('Please Select An Image Or A Frame 😒');
+      }
+      break;
 
-                console.log(imageLink);
-            } else {
-                figma.closePlugin('Please Select An Image Or A Frame 😒');
-            }
-            break;
-
-        default:
-            figma.closePlugin('Please Select An Image Or A Frame 😒');
-            break;
-    }
+    default:
+      figma.closePlugin('Please Select An Image Or A Frame 😒');
+      break;
+  }
 }
 
 const selection = figma.currentPage.selection['0'];
@@ -52,20 +49,20 @@ getContent(selection);
 console.log(selection);
 
 figma.ui.onmessage = event => {
-    if (event.type === 'close') {
-        figma.closePlugin();
-    }
-    if (event.type === 'add-selection') {
-        let selected = figma.currentPage.selection['0'];
-        getContent(selected);
-    }
+  if (event.type === 'close') {
+    figma.closePlugin();
+  }
+  if (event.type === 'add-selection') {
+    let selected = figma.currentPage.selection['0'];
+    getContent(selected);
+  }
 };
 
 //time taken
 const end = Date.now();
 const time = ((end - start) / 1000).toFixed(2);
 figma.ui.postMessage({
-    type: 'time',
-    data: `Completed in : ${time}s`
+  type: 'time',
+  data: `Completed in : ${time}s`
 });
 console.log('Completed in:', time + 's');
--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,136 +1,129 @@
 <template>
-  <div class="brix">
-    <section class="header">
-      <span class="type ">Color Thief </span>
-      <button class='button button--primary' @click="addSelection">Add Selection</button>
-    </section>
+  <div>
+    <header class="c-header">
+      <h4 class="c-header__text c-container">Generate</h4>
+      <hr />
+    </header>
+
+    <main class="c-main c-container">
+      <div class="c-image" v-if="image.src">
+        <p class="c-image__title">{{ image.name }}</p>
+        <div class="c-image__preview">
+          <img :src="image.src" :alt="image.name" />
+        </div>
+      </div>
+
+      <button class="c-generate">
+        Generate Palette
+      </button>
+    </main>
 
     <div class="message" v-if="notify">
       <span>Successfully uploaded</span>
     </div>
     <div class="warning" v-if="warning">
-      <span>{{this.warning}}</span>
+      <span>{{ this.warning }}</span>
     </div>
-
-
-    <div class="image-grid" id="image-holder">
-
-
-    </div>
-
   </div>
 </template>
 
 <script>
-  import "../figma/figma-ds/js/selectMenu";
-  import "../figma/figma-ds/js/iconInput";
-  import "../figma/figma-ds/js/disclosure";
-  import {
+import '../figma/figma-ds/js/selectMenu';
+import '../figma/figma-ds/js/iconInput';
+import '../figma/figma-ds/js/disclosure';
+import { b64toBlob, formatBytes } from './utilis/transformers';
 
-    b64toBlob,
-
-    formatBytes,
-
-  } from "./utilis/transformers";
-
-
-
-
-
-  export default {
-    data() {
-      return {
-        imageArr: [],
-        blob: [],
-        notify: false,
-        warning: null
-      };
-    },
-
-    components: {
-      // Welcome
-    },
-
-    mounted() {
-      // Initialize the figma-ds components
-      window.selectMenu.init();
-      window.iconInput.init();
-      window.disclosure.init();
-
-      window.onmessage = event => {
-        const {
-          type,
-          name,
-          data
-        } = event.data.pluginMessage;
-
-
-
-        if (event.data.pluginMessage.type === 'image') {
-          this.imageArr.push({
-            name: event.data.pluginMessage.name.replace(/\s+/g, ''),
-            image: event.data.pluginMessage.data
-          });
-
-          let b64Data = `${event.data.pluginMessage.data}`
-          this.imageHandler(event, b64Data);
-
-        }
-       
-
-      };
-    },
-    methods: {
-      addSelection() {
-        this.notify = false
-        parent.postMessage({
-            pluginMessage: {
-              type: 'add-selection'
-            }
-          },
-          '*'
-        );
-      },
-      imageHandler(event, b64Data, type) {
-
-
-        var contentType = 'image/png';
-        var blob = b64toBlob(b64Data, contentType);
-        console.log(blob);
-        var blobUrl = URL.createObjectURL(blob);
-
-        // create temporary url 
-        let parent = document.getElementById('image-holder');
-        var div = document.createElement('div');
-        var imageName = document.createElement('span');
-        var input = document.createElement('input');
-        var img = document.createElement('img');
-        img.src = blobUrl;
-        img.height = 200;
-        imageName.className = 'font-sm';
-        imageName.innerHTML =
-          `${event.data.pluginMessage.name.replace(/\s+/g, '') || 'image'} <br/> <b class="bold">Size:</b> ${formatBytes(blob.size)}`;
-       
-        div.appendChild(img);
-        div.appendChild(imageName);
-
-        // 
-        parent.appendChild(div);
-        this.blob = [];
-        this.blob.push({
-          name: event.data.pluginMessage.name,
-          image: blob
-        });
-        console.log('The Blob', this.blob);
-
-
+export default {
+  data() {
+    return {
+      imageArr: [],
+      blob: [],
+      notify: false,
+      warning: null,
+      image: {
+        src: null,
+        name: ''
       }
+    };
+  },
+
+  components: {
+    // Welcome
+  },
+
+  mounted() {
+    // Initialize the figma-ds components
+    window.selectMenu.init();
+    window.iconInput.init();
+    window.disclosure.init();
+
+    window.onmessage = event => {
+      const { type, name, data } = event.data.pluginMessage;
+
+      if (type === 'image') {
+        this.imageArr.push({
+          name: name.replace(/\s+/g, ''),
+          image: data
+        });
+
+        let b64Data = `${data}`;
+        this.imageHandler(event, b64Data);
+      }
+    };
+  },
+  methods: {
+    addSelection() {
+      this.notify = false;
+      parent.postMessage(
+        {
+          pluginMessage: {
+            type: 'add-selection'
+          }
+        },
+        '*'
+      );
+    },
+    imageHandler(event, b64Data, type) {
+      var contentType = 'image/png';
+      var blob = b64toBlob(b64Data, contentType);
+      console.log(blob);
+      var blobUrl = URL.createObjectURL(blob);
+
+      // create temporary url
+      // let parent = document.getElementById('image-holder');
+      // var div = document.createElement('div');
+      // var imageName = document.createElement('span');
+      // var input = document.createElement('input');
+      // var img = document.createElement('img');
+      // img.src = blobUrl;
+      this.image.src = blobUrl;
+      this.image.name = `${event.data.pluginMessage.name.replace(/\s+/g, '') ||
+        'image'}`;
+      // img.height = 200;
+      // imageName.className = 'font-sm';
+      // imageName.innerHTML = `${event.data.pluginMessage.name.replace(
+      //   /\s+/g,
+      //   ''
+      // ) || 'image'} <br/> <b class="bold">Size:</b> ${formatBytes(blob.size)}`;
+
+      // div.appendChild(img);
+      // div.appendChild(imageName);
+
+      //
+      // parent.appendChild(div);
+
+      console.log(this.image);
+      this.blob = [];
+      this.blob.push({
+        name: event.data.pluginMessage.name,
+        image: blob
+      });
+      console.log('The Blob', this.blob);
     }
-
-
-  };
+  }
+};
 </script>
 
 <style lang="scss">
-  @import "../figma/figma-ds/figma-plugin-ds";
+@import '../figma/figma-ds/figma-plugin-ds';
 </style>
